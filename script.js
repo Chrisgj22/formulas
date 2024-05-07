603de8173248fe0da3764528234b69c7d5808919
Chrisgj22 <121605828+Chrisgj22@users.noreply.github.com>
2024-05-07 16:46:43 +1000

    v2.0

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-function calculateD() {
+
+const calc=document.getElementById("c")
+calc.addEventListener("click", function() {
+
     let a = parseFloat(document.getElementById('input_a').value);
     let b = parseFloat(document.getElementById('input_b').value);
 
-    let c = (a * 3.1416 / 4) / 90;
+    let c = (a * 0.7854) / 90;
     let cResult = c * b;
     let dResult = a - cResult;
 
     document.getElementById('output_d').innerText = dResult.toFixed(2);
-    document.getElementById('result_image').style.display = 'block';
-    document.getElementById('result_text').style.display = 'block';
-}
+
+    if (a && b > 0){
+        document.getElementById('result_image').style.display = 'block';
+        document.getElementById('result_text').style.display = 'block';
+    } else {
+        document.getElementById('result_image').style.display = 'none';
+        document.getElementById('result_text').style.display = 'none';
+    }
+
+
+})
